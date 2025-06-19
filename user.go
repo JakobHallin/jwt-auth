442c3827839d70e6baf73863bcde63ed33a639b1
kuber <kuber@kuber.myguest.virtualbox.org>
2025-06-19 07:05:10 -0700

    test for loadusers works

--- user.go
+++ user.go
@@ -1,17 +1,20 @@
 package main
-
-import "fmt"
-
+import (
+	"fmt"
+	"encoding/csv"
+	//"errors"
+	"os"
+	"log"
+)
 type User struct{
 	//Id int  //will not use it now will just have it simple with name and password
-	name string
-	password string //will look how it will be encoded with jws token
+	Name string
+	Password string //will look how it will be encoded with jws token
 }
-
 var users = map[string]User{} //map of the users
-var storage = "user.csv" //
+var storage = "temp.csv" //
 
-func loadUsers(){ //https://www.geeksforgeeks.org/go-language/how-to-read-a-csv-file-in-golang/
+func loadUsers() error{ //https://www.geeksforgeeks.org/go-language/how-to-read-a-csv-file-in-golang/
 	//open file load users
 	file, err := os.Open(storage) //https://pkg.go.dev/os
 	if err != nil {
@@ -19,39 +22,49 @@ func loadUsers(){ //https://www.geeksforgeeks.org/go-language/how-to-read-a-csv-
 		return err
 	}
 	defer file.Close()
-	reader = csv.NewReader(file)
+	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
-		retrurn err
+		return err
 	}
 	for _, eachrecord := range records{
 		fmt.Println(eachrecord)
 	// look at csv how it store to later set in the right
+		if len(eachrecord) < 2 {
+			log.Printf("Skipping invalid record: %v\n", eachrecord)
+			continue
+		}
+		users[eachrecord[0]] = User{
+			Name:     eachrecord[0],
+			Password: eachrecord[1],
+		}
 	}
 	return nil
-	//loop and then set to the map the user should be dynamic cause its go
 }
-
-func saveUsers(user User){
+func saveUser(user User) error{
 	//open file
-	csvFile, err := os.Create("temp.csv")
+	csvFile, err := os.OpenFile("temp.csv", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	//absPath, _ := filepath.Abs(csvFile) //need to make sure it acutaly creates the file
+	fmt.Println("CSV file created at:", csvFile.Name())
 	if err != nil {
 		log.Fatalf("failed creating file: %s", err)
+		return err
 	}
-	csvFile.Close()
+	defer csvFile.Close()
 	//write user to the file
 	writer := csv.NewWriter(csvFile)
-	writer.Flush()
-	return write.Write([]string{user.name, user,password)
+	defer writer.Flush()
+	return writer.Write([]string{user.Name, user.Password})
+	//return writer.Write("test")
 }
 
 //handler in signup should call this function
-func createUser(name, password string) {
+func createUser(name, password string) error{
 	//hash the password
-	user := User{name, password} //id unsure
+	user := User{Name: name, Password: password}
 	//call saveUser(user) to save it
-	users:= user[name]
+	users[name]= user
 	return saveUser(user)
 }
 func auth(name, password string){
--- user_test.go
+++ user_test.go
@@ -1,8 +1,41 @@
 package main
 import (
 	"testing"
+	"os"
 )
 
-func test(){
-	err := createUser("username","password")
+func TestCreateUser(t *testing.T){
+	err := createUser("user","word")
+	if err != nil {
+		t.Errorf("Failed to create user: %v", err)
+	}
+}
+func TestLoadUser(t *testing.T){
+	//Create a temporary CSV file
+	tmpFile, err := os.CreateTemp("", "test_users.csv")
+	if err != nil {
+        	t.Fatalf("Failed to create temp file: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
+	// write to the file the user
+	_, err = tmpFile.WriteString("testuser,password123\n")
+	if err != nil {
+		t.Fatalf("Failed to write to temp file: %v", err)
+	}
+	tmpFile.Close()
+	 //Set the global storage so it dont use the orher file
+	storage = tmpFile.Name()
+	// try to call the loadusers
+	users = make(map[string]User)
+	if err := loadUsers(); err != nil {
+		t.Fatalf("loadUsers failed: %v", err)
+	}
+	// validate
+	user, ok := users["testuser"]
+	if !ok {
+	t.Fatal("Expected user 'testuser' not found")
+	}
+	if user.Password != "password123" {
+        	t.Errorf("Expected password 'password123', got '%s'", user.Password)
+	}
 }
